6001f257f3615159810d7dc46006698bca91c294
Connor Scully-Allison <cscully-allison@nevada.unr.edu>
2018-09-15 14:13:32 -0700

    Modified text.xml with more detail

--- Generator/Classes/Parser.py
+++ Generator/Classes/Parser.py
@@ -6,6 +6,7 @@ class Parser():
 
     def __init__(self, OwlSrc=""):
         self.OwlSrc = OwlSrc
+        self.Graph = Graph()
         self.TripleStore = []
 
     def GetOwlSrc(self):
@@ -25,37 +26,31 @@ class Parser():
         return
 
     def query(self):
-        # queryString = "SELECT * WHERE { ?s ?p ?o. }"
-        #
-        # sparql = SPARQLWrapper("http://dbpedia.org/sparql")
-        # sparql.setQuery("""
-        #     PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
-        #     SELECT ?name
-        #     WHERE {  ?s dbo:isPartOf <http://dbpedia.org/resource/Asturias>.
-        #              ?s foaf:name ?name. }
-        # """)
-        # sparql.setReturnFormat(JSON)
-        # results = sparql.query().convert()
-        #
-        # for result in results["results"]["bindings"]:
-        #     print(u"{} is part of Asturias".format(result['name']["value"]))
 
-        g = Graph()
-        g.parse(self.OwlSrc)
 
-        print(len(g))
+        self.Graph.parse(self.OwlSrc)
 
-        import pprint
-        for trpl in g:
-            pprint.pprint (trpl)
 
+        S = URIRef("http://www.sensor.nevada.edu/ontologies/research_site_hierarchy#site")
         SN = URIRef("http://www.sensor.nevada.edu/ontologies/research_site_hierarchy#site-network")
         OT = URIRef("http://www.sensor.nevada.edu/ontologies/research_site_hierarchy#organizational_tier")
 
         NRDC_RSH = Namespace("https://www.sensor.nevada.edu/ontologies/research_site_hierarchy#")
 
-        # if(SN, RDF.type, OT) in g:
-        #     print("This graph knows that Site Network is an orginizational tier!")
+        # import pprint
+        # for tier in list(self.Graph[:RDF.type:OT]):
+        #     print(tier)
+        #     pprint.pprint(list(self.Graph[tier]))
 
-        for s, p, o in g.triples((None, NRDC_RSH.characteristic, None)):
-            print("%s is characterized by %s"%(s, o))
+
+        for s,p,o in self.Graph.triples((S, NRDC_RSH.characteristic, None)):
+            print("%s is a charactertistic of site"%(o))
+            for su,pr,ob in self.Graph.triples((o, None, None)):
+                print("%s is the %s of %s"%(ob,pr,su))
+
+        # for s, p, o in g.triples((None, NRDC_RSH.parentOf, None)):
+        #     print("%s is parent of %s"%(s, o))
+        #     for su,pr,ob in g.triples((s, NRDC_RSH.characteristic, None)):
+        #         print("The parent of " + o + " is " + s +" and has the characteristic " + ob)
+        #     for su, pr, ob in g.triples((s, NRDC_RSH.ordinalCharacteristic, None)):
+        #         print(ob)
